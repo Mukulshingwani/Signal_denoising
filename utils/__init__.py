45955bb450ff20a0f40f1c262a065fafcff2536a
Nakul Sharma <nakul120.ns@gmail.com>
2021-07-17 02:41:12 +0530

    updated import statements

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,9 @@
-from .ops_utils import conv1d, FT, IFT
+from .ops_utils import conv1d, DFT, IFT
 from .metrics import (
     l2_dist, l1_dist, cos_sim, correlation, energy_dif
 )
 
 __all__ = [
-    'conv1d', 'FT', 'IFT',
+    'conv1d', 'DFT', 'IFT',
     'l2_dist', 'l1_dist', 'cos_sim', 'correlation', 'energy_dif',
 ]
