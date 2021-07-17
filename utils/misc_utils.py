e0e2416107d8795d29de70e3d572d88d8f6572fc
Mukul Shingwani <mukulshingwani@gmail.com>
2021-07-17 13:33:58 +0530

    wrote func for plotting graph

--- utils/misc_utils.py
+++ utils/misc_utils.py
@@ -3,24 +3,18 @@ Plotting the input signal, output signal and the denoised signal
 using matplotlib library for better visualisation
 """
 import matplotlib.pyplot as plt
-
-
-plt.show()
+import numpy as np
 
 
 def graph_plot(x, y):
-    pass
     """
-    for plotting graph
+    for plotting graph with two inputs( same size required as of x-axis)
     """
-    """
-    x_axis = [i for i in range(193)]
+    x_axis = [i for i in range(len(x))]
+    # plt.scatter(parameter_on_x, parameter_on_y, size of dot, color of dot)
     plt.scatter(x_axis, x, s=1, c='blue')
-    plt.show()
     plt.scatter(x_axis, y, s=1, c='red')
     plt.show()
-    plt.xlim(0, 192)
-    plt.show()
-    plt.ylim(0, 50)
-    plt.show()
-    """
+
+
+graph_plot(np.random.randint(1, 1000, 100), np.random.randint(10, 450, 100))
